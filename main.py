ee394fad0a011367573268b166f48c1832d4fc93
unknown <icyveins@hotmail.com>
2021-10-21 00:57:26 +0800

    add debug mode at cmd line, report cmds, start welcome msg

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from dataclasses import dataclass
 import datetime as dt
 from telegram.ext import CommandHandler, CallbackContext
 import telegram
+import sys
 
 #%% Data structs
 @dataclass
@@ -61,6 +62,9 @@ class ARTBot(CommonBot):
         self.dispatcher.add_handler(CommandHandler('name', self.name))
         self.dispatcher.add_handler(CommandHandler('announce', self.announce))
         self.dispatcher.add_handler(CommandHandler('reminders', self.reminders))
+        self.dispatcher.add_handler(CommandHandler('report', self.report))
+        self.dispatcher.add_handler(CommandHandler('reportfull', self.reportfull))
+        self.dispatcher.add_handler(CommandHandler('start', self.start))
         
         print("Added ARTBot's handlers.")
         
@@ -122,7 +126,7 @@ class ARTBot(CommonBot):
         else:
             self.store[user.id] = Submission(' '.join(context.args), None)
             context.bot.send_message(chat_id = update.message.chat_id,
-                                     text = "%s has been added to the list. If you'd like direct message reminders as well, please visit https://telegram.me/seotesterbot" % self.store[user.id].name)
+                                     text = "%s has been added to the list. If you'd like direct message reminders as well, please just open a conversation with me via https://telegram.me/%s" % (self.store[user.id].name, context.bot.username))
             
     def announce(self, update, context):
         self.channel = update.message.chat_id
@@ -137,17 +141,31 @@ class ARTBot(CommonBot):
         context.bot.send_message(chat_id = update.message.chat_id,
                                  text = ''.join(timings))
         
+    def report(self, update, context):
+        txt = "Not submitted this window:\n"
+        for key, val in self.store.items():
+            if val.lastsubmit is None:
+                txt = txt + "%s\n" % val.name
+        
+        context.bot.send_message(chat_id = update.message.chat_id,
+                                 text = txt)
+        
+    def reportfull(self, update, context):
+        self.report(update,context)
+        txt = "Submitted this window:\n"
+        for key, val in self.store.items():
+            if val.lastsubmit is not None:
+                txt = txt + "%s\n" % val.name
+                
+        context.bot.send_message(chat_id = update.message.chat_id,
+                                 text = txt)
+        
+    def start(self, update, context):
+        context.bot.send_message(chat_id = update.message.from_user.id,
+                                 text = "You're all set, you will receive direct message reminders here!")
+        
     #%% Jobs
     def minutely_job(self, context: CallbackContext):
-        # # Reminders (debugging)
-        # if len(self.store.keys()) < 1:
-        #     print("No users yet.")
-        # elif self.open:
-        #     for userid in self.store.keys():
-        #         print("Repeating reminding user %s" % (userid))
-        #         context.bot.send_message(chat_id = userid,
-        #                                  text = "Reminder to update your ART!") # does not work unless user initiated convo with bot beforehand
-        
         # State changes for submission window
         now = dt.datetime.now()
         if not self.open and now > self.openStart:
@@ -183,26 +201,38 @@ class ARTBot(CommonBot):
                 mention = "["+self.store[i].name+"](tg://user?id="+str(i)+")"
                 channeltext = channeltext + mention + "\n"
             
-            context.bot.send_message(chat_id = self.channel,
-                                     text = channeltext,
-                                     parse_mode = telegram.ParseMode.MARKDOWN_V2)
+            if self.channel is not None:
+                context.bot.send_message(chat_id = self.channel,
+                                         text = channeltext,
+                                         parse_mode = telegram.ParseMode.MARKDOWN_V2)
         
         
 #%%
 if __name__ == "__main__":
     reminderTimes = []
-    # # Default reminderTimes
-    # reminderTimes.append((1,7,0))
-    # reminderTimes.append((1,8,0))
-    # reminderTimes.append((1,21,0))
-    # reminderTimes.append((1,22,0))
-    # reminderTimes.append((1,23,0))
-    # reminderTimes.append((1,23,30))
     
-    # Debug reminderTimes
-    now = dt.datetime.now()
-    later = now + dt.timedelta(minutes=1)
-    reminderTimes.append((later.weekday(), later.hour, later.minute))
+    if len(sys.argv) > 1 and sys.argv[1] == 'debug':
+        # Debug reminderTimes
+        now = dt.datetime.now()
+        later = now + dt.timedelta(minutes=1)
+        later2 = later + dt.timedelta(minutes=1)
+        later3 = later2 + dt.timedelta(minutes=1)
+        laterhr = later + dt.timedelta(hours=1)
+        reminderTimes.append((later.weekday(), later.hour, later.minute))
+        print("Debug mode, setting reminder times to shortly after now..")
+        artbot = ARTBot(cutday = later2.weekday(), cuthour = later2.hour, cutmin = later2.minute,
+                         startday = later.weekday(), starthour = later.hour, startmin = later.minute,
+                         reminderTimes = reminderTimes)
+        artbot.run()
+        
+    else:
+        # Default reminderTimes
+        reminderTimes.append((1,7,0))
+        reminderTimes.append((1,8,0))
+        reminderTimes.append((1,21,0))
+        reminderTimes.append((1,22,0))
+        reminderTimes.append((1,23,0))
+        reminderTimes.append((1,23,30))
     
-    artbot = ARTBot(reminderTimes = reminderTimes)
-    artbot.run()
+        artbot = ARTBot(reminderTimes = reminderTimes)
+        artbot.run()
